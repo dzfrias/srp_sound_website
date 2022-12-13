474bbbaf295a89747795033d945c7dc749bb9149
dzfrias <dzfrias@users.noreply.github.com>
2022-12-13 03:27:47 +0000

    deploy: e57375492228958c7821cdd8e6dcc6be7414aa4c

--- srp_sound_website-9c467cdde3f23af9.js
+++ srp_sound_website-9c467cdde3f23af9.js
@@ -657,11 +657,11 @@ function getImports() {
         const ret = wasm.memory;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper196 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper197 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 64, __wbg_adapter_24);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper550 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper551 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 191, __wbg_adapter_27);
         return addHeapObject(ret);
     };
@@ -700,7 +700,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('srp_sound_website-c90ed7d96f9d2f9b_bg.wasm', import.meta.url);
+        input = new URL('srp_sound_website-9c467cdde3f23af9_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
